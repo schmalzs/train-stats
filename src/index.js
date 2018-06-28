8afffc44856562cbf2fadbdc27dc9d95bcafe93e
Scott Schmalz <sschmalz@hubspot.com>
2018-06-28 16:34:22 -0400

    fix problem with default END_DATE

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import jsonexport from 'jsonexport';
 import { getWeekdays } from './lib/dateUtils';
 import { getAllTrains } from './api/trainApi';
 
-const START_DATE = '2017-01-01';
-const END_DATE = null;
+const START_DATE = '2016-12-12';
+const END_DATE = undefined;
 const TRAIN_NUMBERS = ['680', '685', '687'];
-const BATCH_SIZE = 8;
+const BATCH_SIZE = 16;
 const OUTPUT_FILE = '/Users/sschmalz/Desktop/trainData.csv';
 
 const weekdays = getWeekdays(START_DATE, END_DATE);
